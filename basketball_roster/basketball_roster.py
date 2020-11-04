3185bcbaab854f08ec97dc2c06f8d5411372a47f
Juan Sarmiento Burckhardt <juan.burckhardt@coles.com.au>
2020-11-04 13:55:39 +1100

    feat(basket_roster): adding basket roster

---
+++ basketball_roster/basketball_roster.py
@@ -0,0 +1,34 @@
+from random import randrange
+
+# Function to print roster
+def print_roster(team):
+  print("\n\tYour starting 5 for the upcoming basketball season")
+  print(f"\t\t\tPoint Guard:\t\t{team[0]}")
+  print(f"\t\t\tShooting Guard:\t\t{team[1]}")
+  print(f"\t\t\tSmall Forward:\t\t{team[2]}")
+  print(f"\t\t\tPower Forward:\t\t{team[3]}")
+  print(f"\t\t\tCenter:\t\t\t{team[4]}\n")
+
+# Welcome message
+print("Welcome to the Basketball Roster app\n")
+
+# Define team
+team = []
+team.append(input("Who is your point guard: ").title())
+team.append(input("Who is your shooting guard: ").title())
+team.append(input("Who is your small forward: ").title())
+team.append(input("Who is your power forward: ").title())
+team.append(input("Who is your center: ").title())
+print_roster(team)
+
+# Injured player
+bad_luck = randrange(5)
+injured_player = team.pop(bad_luck)
+print(f"Oh no, {injured_player} is injured")
+print("Your roster only has 4 players")
+team.insert(bad_luck,input(f"Who will take {injured_player}'s spot: ").title())
+print_roster(team)
+
+# Wishing luck:
+print(f"Good luck {team[bad_luck]} you will do great!")
+print("Your roster now has 5 players.")
