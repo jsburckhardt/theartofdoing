0fb857a93639a1ca0ad947fce172860a74b646ec
Juan <jsburckhardt@gmail.com>
2020-11-06 08:40:43 +0000

    started codechef

--- codechef/adaanddishes.py
+++ codechef/adaanddishes.py
@@ -3,4 +3,9 @@ with open('adaanddishes.txt') as f:
 
 # loop number of test cases
 # capture test case
-# sort length of plates
+# call time counter
+
+# time counter
+# sort time
+# validate if it is odd or even
+# add last and first 
