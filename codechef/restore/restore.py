d58704a2e989da95dd7f9d97a218cd2fca54db50
Juan <jsburckhardt@gmail.com>
2020-11-11 20:30:47 +1100

    test(find_a): include function for finding a

--- codechef/restore/restore.py
+++ codechef/restore/restore.py
@@ -1,6 +1,7 @@
-import memory_profiler
+# import memory_profiler
+from itertools import product
 
-@profile
+# @profile
 def complex_algorith(n,a,b):
   for i in range(n):
     j = n-1
@@ -10,22 +11,29 @@ def complex_algorith(n,a,b):
         break
       else:
         j -= 1
-    
-    # j_opts = []
-    # for j in range(n):
-    #   if a[j] % a[i] == 0:
-    #     j_opts.append(j+1)
-    # b.append(max(j_opts))
+
+def find_a(n,expected_b):
+  base = [7]*n
+  for i in product(*[range(1,i) for i in base]):
+    b = []
+    complex_algorith(n,i,b)
+    if i[0] == 5:
+      print(f"{i},{b},{expected_b}\n")
+    if tuple(b) == expected_b:
+      print(i)
+    # for j in range(len(count)):
+    #     print( '{0:3} , {1:3} , {2:1}'.format(first_index, table[i[j]][j], j+1))
+    # first_index += 1
 
 
-if __name__ == '__main__':
-  N = 5
-  a = [2,6,5,3,4]
-  # b = [5,2,3,4,5]
-  b = []
-
-  complex_algorith(N,a,b)
-  print(b)
+N = 5
+a = [2,6,5,3,4]
+expected_b = [5,2,3,4,5]
+b = []
+find_a(N,expected_b)
+# complex_algorith(N,a,b)
+# generate_sample_a(4)
+# print(b)
 # if b == processed_b:
 #   print("party my friend")
 
