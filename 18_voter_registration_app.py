42a277096c308efe6406fa558a29a4c2082350dd
Juan Sarmiento Burckhardt <juan.burckhardt@coles.com.au>
2020-11-23 14:56:19 +1100

    chore(voter_registration): make small refactor

--- 18_voter_registration_app.py
+++ 18_voter_registration_app.py
@@ -2,17 +2,15 @@ print("Welcome to the Voter Registration App\n")
 name = input("Please enter your name: ").strip().title()
 age = int(input("Please enter your age: ").strip())
 party_ismajor = False
+parties = ['republican','democratic','independent','libertarian','green']
 
 if age > 17:
   print(f"\nCongratulations {name}! You are old enough to register to vote\n")
   print("Here is a list of political parties to join.")
-  print("\t\t- Republican")
-  print("\t\t- Democratic")
-  print("\t\t- Independent")
-  print("\t\t- Libertarian")
-  print("\t\t- Green")
+  for party in parties:
+    print(f"\t\t- {party.title()}")    
   selected_party = input("\nWhat party would you like to join: ").strip().lower()
-  if selected_party not in ['republican','democratic','independent','libertarian','green']:
+  if selected_party not in parties:
     print("Wrong option!")
     exit(1)
   if selected_party == "republican" or selected_party == "democratic":
