8bde13dd25d6125391d66548f08e59d08e55e02f
Juan Sarmiento Burckhardt <juan.burckhardt@coles.com.au>
2020-11-23 14:52:37 +1100

    chore(script): minor changes

--- 17_coin_toss_app.py
+++ 17_coin_toss_app.py
@@ -4,13 +4,11 @@ print("Welcome to the Shipping Account Program\n")
 
 print("I will flip a coin a set number of times.")
 flip_times = int(input("How many times would you like me to flip the coin: "))
+details = True if input("Would you like to see the result of each flip (y/n): ").lower().strip() == 'y' else False
 
-results = []
 flip_options = ["TAILS","HEADS"]
 tails_total, heads_total = 0,0
-details = True if input("Would you like to see the result of each flip (y/n): ").lower().strip() == 'y' else False
-if details:
-  print("\nFLIPPING!!!\n")
+print("\nFLIPPING!!!\n")
 for _ in range(flip_times):
   if random.choice(flip_options) == 'TAILS':
     tails_total += 1
