1907111de6284e870cecd0f738f0ea7a79c5904e
Juan Sarmiento Burckhardt <juan.burckhardt@coles.com.au>
2021-01-04 04:15:21 +0000

    feat(24): use of collections

--- 24_frequency_analysis.py
+++ 24_frequency_analysis.py
@@ -1,15 +1,24 @@
+from collections import Counter
 import string
+
 # Greeting
 app = "Frequency Analysis"
 print(f"Welcome to the {app} app!")
 
-# Get user data
+# go through 2 use cases
 for phrase_number in range(2):
-    total = 0
+    # acceptable lowecase values to store totals
     result_analysis = dict.fromkeys(string.ascii_lowercase, 0)
-    ordered_letters = [{'_': 0}]
-    sentence = list(input(
-        "\nEnter a word or phrase to count the occurrence of each letter: ").strip().lower())
+    total = 0
+    ordered_letters = [{"_": 0}]
+    # Get user data
+    sentence = list(
+        input(
+            "\nEnter a word or phrase to count the occurrence of each letter: "
+        )
+        .strip()
+        .lower()
+    )
 
     for char in sentence:
         if char.isalnum():
@@ -17,18 +26,20 @@ for phrase_number in range(2):
             total += 1
 
     print(
-        f'\nHere is the frequency analysis from key phrase {phrase_number+1}\n')
-    print('\t\tLetter\t\tOccurrence\t\tPercentage')
-    ordered_items = [{'-': 0}]
-    for character, value in result_analysis.items():
+        f"\nHere is the frequency analysis from key phrase {phrase_number+1}\n"
+    )
+    print("\t\tLetter\t\tOccurrence\t\tPercentage")
+    ordered_items = [{"-": 0}]
+    for character, value in sorted(result_analysis.items()):
         if result_analysis[character] != 0:
             print(
-                f'\t\t{character}\t\t{value}\t\t\t{round(value*100/total,2)}%')
+                f"\t\t{character}\t\t{value}\t\t\t{round(value*100/total,2)}%"
+            )
             for x in range(len(ordered_items)):
                 if value > list(ordered_items[x].values())[0]:
                     ordered_items.insert(x, {character: value})
                     break
     print("\nLetters ordered from highest occurrence to lowest:")
-    for sorted in ordered_items[:-1]:
-        print(list(sorted.keys())[0], end='')
-    print('')
+    for sorted_key in ordered_items[:-1]:
+        print(list(sorted_key.keys())[0], end="")
+    print("")
